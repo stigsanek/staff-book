85693a0aa91d54fa11ca8c8fa2d8bab93cba8173
stigsanek <stigsanek@yandex.ru>
2020-08-07 13:19:27 +0400

    Добавляет конфигурацию кнопок фильтра и данные сотрудников

---
+++ src/data/staff.js
@@ -0,0 +1,113 @@
+export default [
+  {
+    id: 1,
+    photo: 'img/photos/default_avatar.png',
+    name: 'Дейенерис Таргариен',
+    departament: 'Руководство',
+    position: 'Директор',
+    rang: 1,
+    phone: '100',
+    email: 'got@westeros.com'
+  },
+  {
+    id: 2,
+    photo: 'img/photos/default_avatar.png',
+    name: 'Джон Сноу',
+    departament: 'Руководство',
+    position: 'Заместитель директора',
+    rang: 2,
+    phone: '101',
+    email: 'got@westeros.com'
+  },
+  {
+    id: 3,
+    photo: 'img/photos/default_avatar.png',
+    name: 'Тирион Ланнистер',
+    departament: 'Руководство',
+    position: 'Секретарь руководителя',
+    rang: 3,
+    phone: '103',
+    email: 'got@westeros.com',
+    secretary: 'Приемная'
+  },
+  {
+    id: 4,
+    photo: 'img/photos/default_avatar.png',
+    name: 'Санса Старк',
+    departament: 'Отдел продаж',
+    position: 'Начальник отдела',
+    rang: 4,
+    phone: '104',
+    email: 'got@westeros.com'
+  },
+  {
+    id: 5,
+    photo: 'img/photos/default_avatar.png',
+    name: 'Брандон Старк',
+    departament: 'Отдел продаж',
+    position: 'Менеджер по продажам',
+    rang: 6,
+    phone: '105',
+    email: 'got@westeros.com'
+  },
+  {
+    id: 6,
+    photo: 'img/photos/default_avatar.png',
+    name: 'Джейме Ланнистер',
+    departament: 'Отдел разработки',
+    position: 'Начальник отдела',
+    rang: 4,
+    phone: '106',
+    email: 'got@westeros.com'
+  },
+  {
+    id: 7,
+    photo: 'img/photos/default_avatar.png',
+    name: 'Арья Старк',
+    departament: 'Отдел разработки',
+    position: 'Разработчик',
+    rang: 6,
+    phone: '107',
+    email: 'got@westeros.com'
+  },
+  {
+    id: 8,
+    photo: 'img/photos/default_avatar.png',
+    name: 'Сандор Клиган',
+    departament: 'Отдел кадров',
+    position: 'Заместитель начальника отдела',
+    rang: 5,
+    phone: '108',
+    email: 'got@westeros.com'
+  },
+  {
+    id: 9,
+    photo: 'img/photos/default_avatar.png',
+    name: 'Григор Клиган',
+    departament: 'Отдел кадров',
+    position: 'HR-менеджер',
+    rang: 6,
+    phone: '109',
+    email: 'got@westeros.com'
+  },
+  {
+    id: 10,
+    photo: 'img/photos/default_avatar.png',
+    name: 'Серсея Ланнистер',
+    departament: 'Отдел аналитики',
+    position: 'Начальник отдела',
+    rang: 4,
+    phone: '110',
+    email: 'got@westeros.com'
+  },
+  {
+    id: 11,
+    photo: 'img/photos/default_avatar.png',
+    name: 'Джоффри Баратеон',
+    departament: 'Отдел аналитики',
+    position: 'Главный аналитик',
+    rang: 6,
+    phone: '111',
+    email: 'got@westeros.com'
+  }
+];
