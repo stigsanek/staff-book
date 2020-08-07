fcf6413c1b3f64df5269cd0da0eca226cacbdee3
stigsanek <stigsanek@yandex.ru>
2020-08-07 16:00:53 +0400

    Добавляет компонент модального окна

---
+++ src/components/modal/modal.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './modal.css';
+
+const Modal = ({ photo, onModalCLose }) => {
+  const style = {
+    backgroundImage: `url('${photo}')`
+  };
+
+  return (
+    <div className="modal" tabIndex="-1">
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content" style={style}>
+          <button className="close" type="button" onClick={onModalCLose}></button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
