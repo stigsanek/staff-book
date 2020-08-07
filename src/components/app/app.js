ebee698ce17787acc1155172e32cb1d35e8c853b
stigsanek <stigsanek@yandex.ru>
2020-08-07 13:22:44 +0400

    Добавляет компонент app

---
+++ src/components/app/app.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import Header from '../header';
+import SearchField from '../search-field';
+import FilterButtons from '../filter-buttons';
+import EmployeeList from '../employee-list';
+
+import staff from '../../data/staff';
+
+import './app.css';
+
+export default class App extends Component {
+  state = {
+    peoples: staff
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <header className="container mb-5">
+          <div className="navbar justify-content-center mb-3">
+            <Header />
+            <SearchField />
+          </div>
+          <FilterButtons />
+        </header>
+        <main className="container">
+          <EmployeeList data={this.state.peoples} />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
